d95e765c0c348efd8be6198964d9973ec8736bbb
madhur-banger <madhur.cloudevops@gmail.com>
2025-05-15 20:37:53 +0530

    feat: Update README.md

---
+++ python.py
@@ -0,0 +1,7 @@
+# This program says hello and ask for my name and tell lenght onf my name
+print('Hello')
+print('What is your name')
+myName = input()
+print('It is good to meet you' + myName)
+print('Length of your name is:')
+print(len(myName))
